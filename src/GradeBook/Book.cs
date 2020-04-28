969a5454eeb06dddfb8690840aad0d2b1d96709e
tylerdav <tdavis1433@att.net>
2020-04-28 15:40:48 -0500

    finished classes and objects section

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Book
+    {
+        public Book (string name)
+        {
+            grades = new List<double> ();
+            this.name = name;
+        }
+        public void AddGrade (double grade)
+        {
+            grades.Add (grade);
+        }
+        public void ShowStatistics ()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach (var number in grades)
+            {
+                lowGrade = Math.Min (number, lowGrade);
+                highGrade = Math.Max (number, highGrade);
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine ($"the lowest grade is {lowGrade}");
+            Console.WriteLine ($"the highest grade is {highGrade}");
+            Console.WriteLine ($"the average grade is {result:N1}");
+        }
+
+        private List<double> grades;
+        private string name;
+    }
+}
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,50 +7,12 @@ namespace GradeBook
     {
         static void Main (string[] args)
         {
-            // var x = 32.2;
-            // var y = 17.8;
-            // var result = x + y;
-            // Console.WriteLine ($"{x} + {y} = {result}");
+            var book = new Book ("Tyler's Grade Book");
+            book.AddGrade (89.3);
+            book.AddGrade (90.2);
+            book.AddGrade (77.5);
+            book.ShowStatistics ();
 
-            // assigning a new array
-            // var numbers = new double[3];
-            // numbers[0] = 12.7;
-            // numbers[1] = 10.3;
-            // numbers[2] = 6.11;
-
-            // var numbers = new []
-            // {
-            //     12.7,
-            //     10.3,
-            //     6.11,
-            //     4.1
-            // };
-
-            List<double> grades = new List<double> ()
-            {
-                12.7,
-                10.3,
-                6.11,
-                4.1
-            };
-            grades.Add (20.1);
-
-            var result = 0.0;
-            foreach (var number in grades)
-            {
-                result += number;
-            }
-            result /= grades.Count;
-            Console.WriteLine ($"the average grade is {result}");
-
-            if (args.Length > 0)
-            {
-                Console.WriteLine ($"Hello {args[0]}!");
-            }
-            else
-            {
-                Console.WriteLine ($"Hello!");
-            }
         }
     }
 }
