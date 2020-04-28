0754ba56b1ef0b915daf5b28b18c0d1b05ca84e5
tylerdav <tdavis1433@att.net>
2020-04-28 14:37:22 -0500

    finished C# syntax

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,6 +7,42 @@ namespace GradeBook
     {
         static void Main (string[] args)
         {
+            // var x = 32.2;
+            // var y = 17.8;
+            // var result = x + y;
+            // Console.WriteLine ($"{x} + {y} = {result}");
+
+            // assigning a new array
+            // var numbers = new double[3];
+            // numbers[0] = 12.7;
+            // numbers[1] = 10.3;
+            // numbers[2] = 6.11;
+
+            // var numbers = new []
+            // {
+            //     12.7,
+            //     10.3,
+            //     6.11,
+            //     4.1
+            // };
+
+            List<double> grades = new List<double> ()
+            {
+                12.7,
+                10.3,
+                6.11,
+                4.1
+            };
+            grades.Add (20.1);
+
+            var result = 0.0;
+            foreach (var number in grades)
+            {
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine ($"the average grade is {result}");
+
             if (args.Length > 0)
             {
                 Console.WriteLine ($"Hello {args[0]}!");
